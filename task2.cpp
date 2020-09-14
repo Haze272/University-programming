5125ee55d15986496f36fc672a0e598f3ec17fe7
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2020-09-14 10:30:11 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -15,13 +15,14 @@ int main() {
 			throw 100;
 		}
 	}
-	catch (int q) {
+	catch (int q) {  // Когда одно из ислючений проходило в try, то оно отправляло код ошибки, catch принимает код ошибки и обрабатывает её
 		if (q == 100) {
 			cout << "The value you have entered is not correct (from 100 up to 999)" << endl;
 			exit(0);
 		}
 	}
-
+	
+	// Результат получается посредством целочисленного деления введенного значения
 	result = x / 100;
 	cout << "The quantity of 100 is " << result << endl;
 }
