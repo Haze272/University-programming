dd5440901f6940509679e090bb0da7580ed512ed
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2020-09-27 22:12:09 +0300

    2.0.0
    Done with second homework

--- Домашнее задание №2/Source2.cpp
+++ Домашнее задание №2/Source2.cpp
@@ -14,26 +14,27 @@
 
 using namespace std;
 
-int inputInt(int m = INT_MIN, int M = INT_MAX)
-{
-	int N;
+int inputInt(int m, int M) {
 	for (;;) {
-		if ((cin >> N).good() && (m <= N) && (N <= M)) return N;
-
+		int valueChecking;
+		if ((cin >> valueChecking).good() && (m <= valueChecking) && (valueChecking <= M) && (floor(valueChecking) == valueChecking)) {
+			return valueChecking;
+			break;
+		}
 		if (cin.fail()) {
 			cin.clear();
-			cout << "Неверный ввод, повторите." << endl;
+			cout << "Неверный ввод, повторите. \n";
 		}
 		else {
-			cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
+			cout << "Число вне допустимого диапазона значений. Повторите ввод. \n";
 		}
-		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // <-- Я не знаю зачем эта строка, честно не смог понять. Однако из-за max() я не мог поставить windows.h
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	}
 }
-double Inputfloat(int m, int M) {
+
+double inputDouble(int m, int M) {
 	for (;;) {
-		float valuea;
-		cout << "Введите число. \n";
+		double valuea;
 		if ((cin >> valuea).good() && (m <= valuea) && (valuea <= M)) {
 			return valuea;
 			break;
@@ -53,92 +54,23 @@ void task2_1() {
 	system("cls");
 	string command_empty;
 	bool isExit = false;
-	double coneMinimum = 0;
-	double coneMaximum = 2147483647;
 
 	cout << "\n\n\n\n\n\n\n\n\n\n----------------Конус----------------\n\n\n\n\n\n\n\n\n\n";
 
 	double Volume, Area, height, radiusOut, radiusIn, forming;
 
 	do {
-		/*
-		while (true) {
-			float localradiusOut;
-			cout << "Введите радиус большего основания:" << endl;
-			if ((cin >> localradiusOut).good() && (coneMinimum <= localradiusOut) && (localradiusOut <= coneMaximum)) {
-				radiusOut = localradiusOut;
-				break;
-			}
+		cout << "Введите радиус большего основания:\n";
+		radiusOut = inputDouble(0, 2147483647);
 
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-		*/
-		cout << "Введите радиус большего основания:" << endl;
-		radiusOut = inputInt(0, 2147483647);
+		cout << "Введите радиус меньшего основания:\n";
+		radiusIn = inputDouble(0, 2147483647);
 
-		cout << "Введите радиус меньшего основания:" << endl;
-		radiusIn = inputInt(0, 2147483647);
-		/*
-		while (true) {
-			float localradiusIn;
-			cout << "Введите радиус меньшего основания:" << endl;
-			if ((cin >> localradiusIn).good() && (coneMinimum <= localradiusIn) && (localradiusIn <= coneMaximum)) {
-				radiusIn = localradiusIn;
-				break;
-			}
+		cout << "Введите высоту:\n";
+		height = inputDouble(0, 2147483647);
 
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-		*/
-		while (true) {
-			float localheight;
-			cout << "Введите высоту:" << endl;
-			if ((cin >> localheight).good() && (coneMinimum <= localheight) && (localheight <= coneMaximum)) {
-				height = localheight;
-				break;
-			}
-
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-
-		while (true) {
-			float localforming;
-			cout << "Введите образующую конуса:" << endl;
-			if ((cin >> localforming).good() && (coneMinimum <= localforming) && (localforming <= coneMaximum)) {
-				forming = localforming;
-				break;
-			}
-
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
+		cout << "Введите образующую конуса:\n";
+		forming = inputDouble(0, 2147483647);
 
 		system("cls");
 
@@ -160,71 +92,39 @@ void task2_2() {
 	system("cls");
 	string command_empty;
 	bool isExit = false;
-	double coneMinimum = 0;
-	double coneMaximum = 2147483647;
 
 	double w, a, x;
 
 	cout << "\n\n\n\n\n\n\n\n\n\n----------------Разветвление----------------\n\n\n\n\n\n\n\n\n\n";
 
 	do {
-		while (true) {
-			float localParameter;
-			cout << "Введите значение параметра a:" << endl;
-			if ((cin >> localParameter).good() && (coneMinimum <= localParameter) && (localParameter <= coneMaximum)) {
-				a = localParameter;
-				break;
-			}
+		cout << "Введите значение параметра a:" << endl;
+		a = inputDouble(-2147483648, 2147483647);
 
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-
-		while (true) {
-			float localVariable;
-			cout << "Введите значение переменной x:" << endl;
-			if ((cin >> localVariable).good() && (coneMinimum <= localVariable) && (localVariable <= coneMaximum)) {
-				x = localVariable;
-				break;
-			}
-
-			if (cin.fail()) {
-				cin.clear();
-				cout << "Неверный ввод, повторите." << endl;
-			}
-			else {
-				cout << "Число вне допустимого диапазона значений. Повторите ввод.\n";
-			}
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
+		cout << "Введите значение переменной x:" << endl;
+		x = inputDouble(-2147483648, 2147483647);
 
 		try {
 			if (abs(x) <= 0 && a - pow(x, 2) < 0) {
-				throw 5001;
+				throw 5201;
 			}
 			if (abs(x) <= 0) {
-				throw 5002;
+				throw 5202;
 			}
 			if (a - pow(x, 2) < 0) {
-				throw 5003;
+				throw 5203;
 			}
 		}
 		catch (int dripperCatcher) {
-			if (dripperCatcher == 5001) {
+			if (dripperCatcher == 5201) {
 				cout << "Вы вышли за область предела функции (|x| > 0) и (a - x^2 >= 0)";
 				break;
 			}
-			if (dripperCatcher == 5002) {
+			if (dripperCatcher == 5202) {
 				cout << "Вы вышли за область предела функции (|x| > 0)";
 				break;
 			}
-			if (dripperCatcher == 5003) {
+			if (dripperCatcher == 5203) {
 				cout << "Вы вышли за область предела функции (a - x^2 >= 0)";
 				break;
 			}
@@ -236,8 +136,8 @@ void task2_2() {
 		else if (abs(x) >= 1) {
 			w = sqrt(a - pow(x, 2));
 		}
+		cout << "\n\n\n\nОтвет равен " << w;
 
-		cout << "\n\n\n\n Ответ равен " << w;
 		cout << "\nПродолжим? Напишите всё что угодно для продолжения, !exit если хотите выйти из программы\n";
 		cin >> command_empty;
 		if (command_empty == "!exit") {
@@ -250,24 +150,125 @@ void task2_3() {
 	system("cls");
 	string command_empty;
 	bool isExit = false;
-	double coneMinimum = 0;
-	double coneMaximum = 2147483647;
 
-	double w, a, x;
+	double z, b, x, y;
 
 	cout << "\n\n\n\n\n\n\n\n\n\n----------------Функция----------------\n\n\n\n\n\n\n\n\n\n";
 
 	do {
+		cout << "Введите значение параметра b:" << endl;
+		b = inputDouble(-2147483648, 2147483647);
 
+		cout << "Введите значение переменной x:" << endl;
+		x = inputDouble(-2147483648, 2147483647);
+
+		cout << "Введите значение переменной y:" << endl;
+		y = inputDouble(-2147483648, 2147483647);
+
+		try {
+			if (b - y <= 0 && b - x < 0) {
+				throw 5301;
+			}
+			if (b - y <= 0) {
+				throw 5302;
+			}
+			if (b - x < 0) {
+				throw 5303;
+			}
+		}
+		catch (int dripperCatcher) {
+			if (dripperCatcher == 5301) {
+				cout << "Вы вышли за область предела функции (b - y > 0) и (b - x >= 0)";
+				break;
+			}
+			if (dripperCatcher == 5302) {
+				cout << "Вы вышли за область предела функции (b - y > 0)";
+				break;
+			}
+			if (dripperCatcher == 5303) {
+				cout << "Вы вышли за область предела функции (b - x >= 0)";
+				break;
+			}
+		}
+
+		z = log(b - y) * sqrt(b - x);
+		cout << "\n\n\n\nФункция равна " << z;
+
+		cout << "\nПродолжим? Напишите всё что угодно для продолжения, !exit если хотите выйти из программы\n";
+		cin >> command_empty;
+		if (command_empty == "!exit") {
+			isExit = true;
+		}
 	} while (!isExit);
 }
 
 void task2_4() {
-	cout << "Мы из антихайпа!!! 4";
+	system("cls");
+	string command_empty;
+	bool isExit = false;
+
+	int order;
+
+	cout << "\n\n\n\n\n\n\n\n\n\n----------------Порядок----------------\n\n\n\n\n\n\n\n\n\n";
+
+	do {
+		cout << "Введите число, для которого выведутся последующие 10\n";
+
+		order = inputInt(0, 2147483647);
+		cout << "Последовательность равна:\n";
+		for (int i = 10; i > 0; i--) {
+			order++;
+			cout << order << endl;
+		}
+
+		cout << "\nПродолжим? Напишите всё что угодно для продолжения, !exit если хотите выйти из программы\n";
+		cin >> command_empty;
+		if (command_empty == "!exit") {
+			isExit = true;
+		}
+	} while (!isExit);
 }
 
 void task2_5() {
-	cout << "Мы из антихайпа!!! 5";
+	system("cls");
+	string command_empty;
+	bool isExit = false;
+
+	double tabel, result;
+
+	cout << "\n\n\n\n\n\n\n\n\n\n----------------Табуляция----------------\n\n\n\n\n\n\n\n\n\n";
+
+	do {
+		cout << "Введите число для табулирования функции\n";
+		tabel = inputDouble(-2147483648, 2147483647);
+		cout << endl;
+		tabel -= 4;
+		int counter = 1;
+
+		for (int i = 0; i < 17; i++) {
+			cout << counter << ") ";
+			if (tabel - 1 != 0) {
+				result = (pow(tabel, 2) - 2 * tabel + 2) / (tabel - 1);
+				cout << "Результат для x = " << tabel << "равен: " << result << endl << endl;
+			}
+			try {
+				if (tabel - 1 == 0) {
+					throw 5501;
+				}
+			}
+			catch (int q) {
+				cout << "Результат для x = 1\n" << "Ошибка, на 0 делить нельзя (x - 1 != 0)" << endl << endl;
+			}
+			tabel += 0.5;
+			counter++;
+		}
+
+		cout << "\nПродолжим? Напишите всё что угодно для продолжения, !exit если хотите выйти из программы\n";
+		cin >> command_empty;
+		if (command_empty == "!exit") {
+			isExit = true;
+		}
+	} while (!isExit);
 }
 
 int main() {
@@ -289,12 +290,12 @@ int main() {
 		cout << "№1 - задание \"Конус»\"\n";
 		cout << "№2 - задание \"Разветвление\"\n";
 		cout << "№3 - задание \"Функция\"\n";
-		cout << "№4 - задание \"Порядок»\"\n";
+		cout << "№4 - задание \"Порядок\"\n";
 		cout << "№5 - задание \"Табуляция\"\n\n";
 		cout << "Ввод нуля завершит программу\n\n";
 
 		// Нас интересует проверка, которая будет определять, ввёл ли пользователь число от 1 до 5.
-		taskNumber = inputInt(1, 5);
+		taskNumber = inputInt(0, 5);
 
 		// Когда пользователь вводит номер задания и соответствующий case реазилуется, после этого работа главной функции не завешается и это может привести к ошибкам.
 		// Поэтому сразу как только функция - задание выполнится, будет выход из switch.
